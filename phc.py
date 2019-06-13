fcb4c9b24a779e823eddc7a2c55af2dfa0992ba2
Momchil <momchilmm@gmail.com>
2019-06-13 15:56:11 -0700

    Added a number of plotting capabilities and some additional methods

--- phc.py
+++ phc.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from utils import *
 
 # Class for a photonic crystal which can contain a number of layers
 class PhotCryst(object):
@@ -31,49 +31,101 @@ class PhotCryst(object):
 		layer = Layer(self.lattice, z_min, z_min + d, eps_b=eps_b)
 		self.layers.append(layer)
 
-	def add_hole(self, shape, params, layer_ind=0):
+	def add_shape(self, sh_type, params, layer_ind=0):
 		'''
-		Add a hole to layer number layer_ind
+		Add a shape to layer number layer_ind
 		'''
 		if layer_ind >= len(self.layers):
 			raise(ValueError("Layer index larger than total number of layers"))
 		else:
-			self.layers[layer_ind].add_hole(shape, params)
+			self.layers[layer_ind].add_shape(sh_type, params)
 
-	def plot_xz(self, dx=2e-2, dz=2e-2):
+	def get_eps(self, points):
 		'''
-		Plot an xz-cross section showing all the layers and holes
+		Compute the permittivity of the PhC at a set of points defined by
+		a tuple of x, y, z positions which are same-size arrays
 		'''
-		zmin = self.layers[0].z_min - 1
-		zmax = self.layers[-1].z_max + 1
+		(xmesh, ymesh, zmesh) = points
+		a_shape = xmesh.shape
+		if (ymesh.shape != a_shape) or (ymesh.shape != a_shape):
+			raise (ValueError(
+					"xmesh, ymesh and zmesh must have the same shape"))
 
-		xmax = np.abs(max([self.lattice['a1'][0], self.lattice['a2'][0]]))
-		xmin = -xmax
-
-		nx = int((xmax - xmin)//dx)
-		nz = int((zmax - zmin)//dz)
-
-		xgrid = xmin + np.arange(nx)*dx
-		zgrid = zmin + np.arange(nz)*dz
-
-		[xmesh, zmesh] = np.meshgrid(xgrid, zgrid)
- 
-		eps_r = np.zeros((nz, nx))
+		eps_r = np.zeros(a_shape)
 
 		eps_r[zmesh < self.layers[0].z_min] = self.eps_l
 		eps_r[zmesh > self.layers[-1].z_max] = self.eps_u
+		a1 = self.lattice['a1']
+		a2 = self.lattice['a2']
 
 		for layer in self.layers:
 			zlayer = (zmesh >= layer.z_min) * (zmesh < layer.z_max)
 			eps_r[zlayer] = layer.eps_b
-			for hole in layer.holes:
-				xhole = hole.is_inside(xmesh, np.zeros(np.shape(xmesh)))
-				eps_r[xhole*zlayer] = hole.eps_h
 
-		plt.imshow(eps_r, cmap = "Greys", origin='lower',
-						 extent=[xmin, xmax, zmin, zmax])
-		plt.colorbar()
-		plt.show()
+			# Slightly hacky way to include the periodicity
+			a_p = min([np.linalg.norm(a1), 
+					   np.linalg.norm(a2)])
+			nmax = int(np.sqrt(np.square(np.max(abs(xmesh))) + 
+							np.square(np.max(abs(ymesh))))/a_p) + 1
+
+			for shape in layer.shapes:
+				for n1 in range(-nmax, nmax):
+					for n2 in range(-nmax, nmax):
+						in_shape = shape.is_inside(xmesh + 
+							n1*a1[0] + n2*a2[0], ymesh + 
+							n1*a1[1] + n2*a2[1])
+						eps_r[in_shape*zlayer] = shape.eps
+
+		return eps_r
+
+	def get_eps_bounds(self):
+		# Returns the minimum and maximum permittivity of the structure
+		eps_min = min([self.eps_l, self.eps_u])
+		eps_max = max([self.eps_l, self.eps_u])
+
+		for layer in self.layers:
+			eps_min = min([eps_min, layer.eps_b])
+			eps_max = max([eps_max, layer.eps_b])
+			for shape in layer.shapes:
+				eps_min = min([eps_min, shape.eps])
+				eps_max = max([eps_max, shape.eps])
+
+		return (eps_min, eps_max)
+
+	def plot_cross(self, cross_section='xy', pos=0, res=[2e-2, 2e-2]):
+		'''
+		Plot a cross-section of the PhC at position pos along the third axis
+		'''
+		if cross_section == 'xy':
+			plot_xy(self, z=pos, dx=res[0], dy=res[1])
+		elif cross_section == 'xz':
+			plot_xz(self, y=pos, dx=res[0], dz=res[1])
+		elif cross_section == 'yz':
+			plot_yz(self, x=pos, dy=res[0], dz=res[1])
+		else:
+			raise(ValueError("Cross-section must be in {'xy', 'yz', 'xz'}"))
+
+	def plot_overview(self, res=[1e-2, 1e-2, 2e-2]):
+		'''
+		Plot an overview of PhC cross-sections
+		'''
+
+		(eps_min, eps_max) = self.get_eps_bounds()
+		fig, ax = plt.subplots(1, 2, constrained_layout=True)
+		plot_xz(self, ax=ax[0], dx=res[0], dz=res[2],
+					clim=[eps_min, eps_max], cbar=False)
+		ax[0].set_title("xz-view")
+		plot_yz(self, ax=ax[1], dy=res[1], dz=res[2],
+					clim=[eps_min, eps_max], cbar=True)
+		ax[1].set_title("yz-view")
+
+		N_layers = len(self.layers)
+		fig, ax = plt.subplots(1, N_layers, constrained_layout=True)
+		for indl in range(N_layers):
+			zpos = (self.layers[indl].z_max + self.layers[indl].z_min)/2
+			plot_xy(self, z=zpos, ax=ax[indl], dx=res[0], dy=res[1],
+					clim=[eps_min, eps_max], cbar=indl==N_layers-1)
+			ax[indl].set_title("xy in layer %d" % indl)
 
 
 # Class for a single layer in the PhC
@@ -92,32 +144,32 @@ class Layer(object):
 		# Initialize average permittivity - needed for guided-mode computation
 		self.eps_avg = eps_b
 
-		# Initialize an empty list of holes
-		self.holes = []
+		# Initialize an empty list of shapes
+		self.shapes = []
 
-	def add_hole(self, shape, params):
-		# Add a hole with permittivity eps_h to the layer
-		if shape == 'circle':
-			hole = Circle(params['eps_h'], params['x'], 
+	def add_shape(self, sh_type, params):
+		# Add a shape with permittivity eps to the layer
+		if sh_type == 'circle':
+			shape = Circle(params['eps'], params['x'], 
 							params['y'], params['r'])
 		else:
-			raise(NotImplementedError("Hole must be one of {'circle',}"))
+			raise(NotImplementedError("Shape must be one of {'circle',}"))
 
-		self.holes.append(hole)
-		self.eps_avg = (self.eps_avg*(self.lattice['ec_area']- hole.area) + 
-						hole.eps_h*hole.area)/self.lattice['ec_area']
+		self.shapes.append(shape)
+		self.eps_avg = (self.eps_avg*(self.lattice['ec_area'] - shape.area) + 
+						shape.eps*shape.area)/self.lattice['ec_area']
 
 	# NB: to implement method to compute FT of permittivity 
 
 
 class Circle(object):
 	'''
-	Define class for a circular hole
-	Other types of holes classes can also be added; they need to compute and 
-	store the area of the hole and to have a method to compute the in-plane FT 
+	Define class for a circular shape
+	Other types of shape classes should also be added; they need to compute and 
+	store the area of the shape and to have a method to compute the in-plane FT 
 	'''
-	def __init__(self, eps_h=1, x=0, y=0, r=0):
-		self.eps_h=eps_h
+	def __init__(self, eps=1, x=0, y=0, r=0):
+		self.eps=eps
 		self.x = x
 		self.y = y
 		self.r = r
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def init_lattice(a1=np.array([1, 0]), a2=np.array([0, 1])):
 	'''
@@ -20,3 +21,89 @@ def init_lattice(a1=np.array([1, 0]), a2=np.array([0, 1])):
 				'bz_area': bz_area}
 
 	return lattice
+
+def plot_eps(eps_r, clim=None, ax=None, extent=None, cmap="Greys", cbar=False):
+
+	if ax is None:
+		fig, ax = plt.subplots(1, constrained_layout=True)
+
+	im = ax.imshow(eps_r, cmap=cmap, origin='lower', extent=extent)
+	if clim:
+		im.set_clim(vmin=clim[0], vmax=clim[1])
+
+	if cbar:
+		plt.colorbar(im)
+
+def plot_xz(phc, y=0, dx=2e-2, dz=2e-2, ax=None, clim=None, cbar=False):
+	'''
+	Plot an xz-cross section showing all the layers and shapes
+	'''
+	zmin = phc.layers[0].z_min - 1
+	zmax = phc.layers[-1].z_max + 1
+
+	xmax = np.abs(max([phc.lattice['a1'][0], phc.lattice['a2'][0]]))
+	xmin = -xmax
+
+	nx = int((xmax - xmin)//dx)
+	nz = int((zmax - zmin)//dz)
+
+	xgrid = xmin + np.arange(nx)*dx
+	zgrid = zmin + np.arange(nz)*dz
+
+	[xmesh, zmesh] = np.meshgrid(xgrid, zgrid)
+	ymesh = y*np.ones(xmesh.shape)
+
+	eps_r = phc.get_eps((xmesh, ymesh, zmesh))
+	extent = [xmin, xmax, zmin, zmax]
+
+	plot_eps(eps_r, clim=clim, ax=ax, extent=extent, cbar=cbar)
+
+
+def plot_xy(phc, z=0, dx=2e-2, dy=2e-2, ax=None, clim=None, cbar=False):
+	'''
+	Plot an xy-cross section showing all the layers and shapes
+	'''
+	ymax = np.abs(max([phc.lattice['a1'][1], phc.lattice['a2'][1]]))
+	ymin = -ymax
+
+	xmax = np.abs(max([phc.lattice['a1'][0], phc.lattice['a2'][0]]))
+	xmin = -xmax
+
+	nx = int((xmax - xmin)//dx)
+	ny = int((ymax - ymin)//dy)
+
+	xgrid = xmin + np.arange(nx)*dx
+	ygrid = ymin + np.arange(ny)*dy
+
+	[xmesh, ymesh] = np.meshgrid(xgrid, ygrid)
+	zmesh = z*np.ones(xmesh.shape)
+
+	eps_r = phc.get_eps((xmesh, ymesh, zmesh))
+	extent = [xmin, xmax, ymin, ymax]
+
+	plot_eps(eps_r, clim=clim, ax=ax, extent=extent, cbar=cbar)
+
+
+def plot_yz(phc, x=0, dy=2e-2, dz=2e-2, ax=None, clim=None, cbar=False):
+	'''
+	Plot a yz-cross section showing all the layers and shapes
+	'''
+	zmin = phc.layers[0].z_min - 1
+	zmax = phc.layers[-1].z_max + 1
+
+	ymax = np.abs(max([phc.lattice['a1'][1], phc.lattice['a2'][1]]))
+	ymin = -ymax
+
+	ny = int((ymax - ymin)//dy)
+	nz = int((zmax - zmin)//dz)
+
+	ygrid = ymin + np.arange(ny)*dy
+	zgrid = zmin + np.arange(nz)*dz
+
+	[ymesh, zmesh] = np.meshgrid(ygrid, zgrid)
+	xmesh = x*np.ones(ymesh.shape)
+
+	eps_r = phc.get_eps((xmesh, ymesh, zmesh))
+	extent = [ymin, ymax, zmin, zmax]
+
+	plot_eps(eps_r, clim=clim, ax=ax, extent=extent, cbar=cbar)
