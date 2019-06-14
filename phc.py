3ba177974093f12ca1e4215e9e78085172c8058a
Momchil <momchilmm@gmail.com>
2019-06-14 14:05:12 -0700

    Made lattice a class and it can be square, hexagonal or arbitrary (through primitive vectors)

--- phc.py
+++ phc.py
@@ -56,8 +56,8 @@ class PhotCryst(object):
 
 		eps_r[zmesh < self.layers[0].z_min] = self.eps_l
 		eps_r[zmesh >= self.layers[-1].z_max] = self.eps_u
-		a1 = self.lattice['a1']
-		a2 = self.lattice['a2']
+		a1 = self.lattice.a1
+		a2 = self.lattice.a2
 
 		for layer in self.layers:
 			zlayer = (zmesh >= layer.z_min) * (zmesh < layer.z_max)
@@ -134,8 +134,10 @@ class PhotCryst(object):
 			ax[indl].set_title("xy in layer %d" % indl)
 
 
-# Class for a single layer in the PhC
 class Layer(object):
+	'''
+	Class for a single layer in the potentially multi-layer PhC
+	'''
 	def __init__(self, lattice, z_min, z_max, eps_b=1):
 		# Define beginning and end in z-direction
 		self.z_min = z_min
@@ -169,5 +171,58 @@ class Layer(object):
 						{'circle', 'square', 'poly'}"))
 
 		self.shapes.append(shape)
-		self.eps_avg = (self.eps_avg*(self.lattice['ec_area'] - shape.area) + 
-						shape.eps*shape.area)/self.lattice['ec_area']
+		self.eps_avg = (self.eps_avg*(self.lattice.ec_area - shape.area) + 
+						shape.eps*shape.area)/self.lattice.ec_area
+
+
+class Lattice(object):
+	'''
+	Class for constructing a Bravais lattice
+	'''
+	def __init__(self, *args):
+		'''
+		Initialize a Bravais lattice.
+		If a single argument is passed, then
+			- 'square': initializes a square lattice
+			- 'hexagonal': initializes a hexagonal lattice
+			(lattice constant 1 in both cases)
+		If two arguments are passed, they should each be 2-element arrays
+		defining the elementary vectors of the lattice
+		'''
+
+		(a1, a2) = self.parse_input(*args)
+		self.a1 = a1
+		self.a2 = a2
+
+		ec_area = np.linalg.norm(np.cross(a1, a2))
+		a3 = np.array([0, 0, 1])
+
+		b1_3d = 2*np.pi*np.cross(a2, a3)[0:2]/np.dot(a1, np.cross(a2, a3)[0:2]) 
+		b2_3d = 2*np.pi*np.cross(a3, a1)[0:2]/np.dot(a2, np.cross(a3, a1)[0:2])
+
+		bz_area = np.linalg.norm(np.cross(b1_3d, b2_3d))
+
+		# Reciprocal lattice vectors
+		self.b1 = b1_3d[0:2]
+		self.b2 = b2_3d[0:2]
+
+		self.ec_area = ec_area	# Elementary cell area
+		self.bz_area = bz_area	# Brillouin zone area
+
+	def parse_input(self, *args):
+		if len(args) == 1:
+			if args[0] == 'square':
+				a1 = np.array([1, 0])
+				a2 = np.array([0, 1])
+			elif args[0] == 'hexagonal':
+				a1 = np.array([0.5, np.sqrt(3)/2])
+				a2 = np.array([0.5, -np.sqrt(3)/2])
+			else:
+				raise(ValueError, "Lattice can be 'square' or 'hexagonal, \
+					or defined through two primitive vectors.")
+
+		elif len(args) == 2:
+			a1 = args[0]
+			a2 = args[1]
+
+		return (a1, a2)
--- utils.py
+++ utils.py
@@ -1,27 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def init_lattice(a1=np.array([1, 0]), a2=np.array([0, 1])):
-	'''
-	Initialize a Bravais lattice; default is square
-	'''
-	ec_area = np.linalg.norm(np.cross(a1, a2))
-	a3 = np.array([0, 0, 1])
-
-	b1_3d = 2*np.pi*np.cross(a2, a3)[0:2]/np.dot(a1, np.cross(a2, a3)[0:2]) 
-	b2_3d = 2*np.pi*np.cross(a3, a1)[0:2]/np.dot(a2, np.cross(a3, a1)[0:2])
-
-	bz_area = np.linalg.norm(np.cross(b1_3d, b2_3d))
-
-	lattice = {	'a1': a1,
-				'a2': a2,
-				'b1': b1_3d[0:2],
-				'b2': b2_3d[0:2],
-				'ec_area': ec_area,
-				'bz_area': bz_area}
-
-	return lattice
-
 def plot_eps(eps_r, clim=None, ax=None, extent=None, cmap="Greys", cbar=False):
 
 	if ax is None:
@@ -41,7 +20,7 @@ def plot_xz(phc, y=0, dx=2e-2, dz=2e-2, ax=None, clim=None, cbar=False):
 	zmin = phc.layers[0].z_min - 1
 	zmax = phc.layers[-1].z_max + 1
 
-	xmax = np.abs(max([phc.lattice['a1'][0], phc.lattice['a2'][0]]))
+	xmax = np.abs(max([phc.lattice.a1[0], phc.lattice.a2[0]]))
 	xmin = -xmax
 
 	nx = int((xmax - xmin)//dx)
@@ -63,10 +42,10 @@ def plot_xy(phc, z=0, dx=2e-2, dy=2e-2, ax=None, clim=None, cbar=False):
 	'''
 	Plot an xy-cross section showing all the layers and shapes
 	'''
-	ymax = np.abs(max([phc.lattice['a1'][1], phc.lattice['a2'][1]]))
+	ymax = np.abs(max([phc.lattice.a1[1], phc.lattice.a2[1]]))
 	ymin = -ymax
 
-	xmax = np.abs(max([phc.lattice['a1'][0], phc.lattice['a2'][0]]))
+	xmax = np.abs(max([phc.lattice.a1[0], phc.lattice.a2[0]]))
 	xmin = -xmax
 
 	nx = int((xmax - xmin)//dx)
@@ -91,7 +70,7 @@ def plot_yz(phc, x=0, dy=2e-2, dz=2e-2, ax=None, clim=None, cbar=False):
 	zmin = phc.layers[0].z_min - 1
 	zmax = phc.layers[-1].z_max + 1
 
-	ymax = np.abs(max([phc.lattice['a1'][1], phc.lattice['a2'][1]]))
+	ymax = np.abs(max([phc.lattice.a1[1], phc.lattice.a2[1]]))
 	ymin = -ymax
 
 	ny = int((ymax - ymin)//dy)
