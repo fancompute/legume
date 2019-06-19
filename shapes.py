f1694e1f995b2f056e235a8ff6833be4c600792d
momchilmm <momchilmm@gmail.com>
2019-06-18 18:17:45 -0700

    Polygon FT ported but needs some debugging

--- shapes.py
+++ shapes.py
@@ -57,7 +57,7 @@ class Poly(Shape):
 		self.x_edges.append(x_edges[0])
 		self.y_edges.append(y_edges[0])
 
-		self.area = 1 # FIX THIS
+		self.area = np.real(self.compute_ft(np.array([[0], [0]])))
 
 	def compute_ft(self, gvec):
 		'''
@@ -65,12 +65,50 @@ class Poly(Shape):
 		NB: the vertices of the polygonal should be defined in a 
 		counter-clockwise manner!
 		Input: 
-			- gvec: [Ng x 2] numpy array
+			- gvec: [2 x Ng] numpy array
 		'''
-		gx = gvec[:, 0]
-		gy = gvec[:, 1]
-		# To be done...
+		gx = np.array(gvec[0, :])
+		gy = np.array(gvec[1, :])
+		xj = np.array(self.x_edges)
+		yj = np.array(self.y_edges)
+		npts = xj.shape[0]
+		ng = gx.shape[0]
+		gx = gx[:, np.newaxis]
+		gy = gy[:, np.newaxis]
+		xj = xj[np.newaxis, :]
+		yj = yj[np.newaxis, :]
 
+		ft = np.zeros((ng));
+
+		aj = (np.roll(xj, -1, axis=1) - xj + 1e-10) / \
+				(np.roll(yj, -1, axis=1) - yj + 1e-10)
+		bj = xj - aj * yj
+		bgtemp = gx.dot(bj)
+		agtemp1 = gy.dot(yj) + gx.dot(aj * yj)
+		agtemp2 = gy.dot(np.roll(yj, -1, axis=1)) + \
+					gx.dot(aj * np.roll(yj, -1, axis=1))
+		ft = -np.sum(np.exp(1j*bgtemp) * (np.exp(1j * agtemp2) - \
+					np.exp(1j * agtemp1)) / (gx * (gy + gx.dot(aj)) + 1e-10), 
+					axis=1)
+		a2j = 1 / aj
+		b2j = yj - a2j * xj
+		ind_gx0 = np.where(np.abs(gx[:, 0]) < 1e-10)[0]
+		if ind_gx0.size > 0:
+			bgtemp = gy[ind_gx0, :] * b2j
+			agtemp1 = gx[ind_gx0, :].dot(xj) + gy[ind_gx0, :].dot(a2j * xj)
+			agtemp2 = gx[ind_gx0, :].dot(np.roll(xj, -1, axis=1)) + \
+					gy[ind_gx0, :].dot(a2j * np.roll(xj, -1, axis=1));
+			ft[ind_gx0] = np.sum(np.exp(1j*bgtemp) * (np.exp(1j*agtemp2) - \
+					np.exp(1j*agtemp1)) / (gy[ind_gx0, :] * (gx[ind_gx0, :] + \
+					gy[ind_gx0, :].dot(a2j)) + 1e-10), axis=1);
+
+		ind_gxy0 = np.where((np.abs(gx[:, 0]) < 1e-10) * 
+							(np.abs(gx[:, 0]) < 1e-10))[0]
+		if ind_gxy0.size > 0:
+			ft[ind_gxy0] = np.sum(xj * np.roll(yj, -1, axis=1) - \
+						yj * np.roll(xj, -1, axis=1))/2;
+		
+		return ft
 
 	def is_inside(self, x, y):
 		vert = np.vstack((np.array(self.x_edges), np.array(self.y_edges)))
@@ -86,6 +124,6 @@ class Square(Poly):
 	def __init__(self, eps=1, x_cent=0, y_cent=0, a=0):
 		self.x_cent = x_cent
 		self.y_cent = y_cent
-		x_edges = [x_cent - a/2, x_cent - a/2, x_cent + a/2, x_cent + a/2]
-		y_edges = [y_cent - a/2, y_cent + a/2, y_cent + a/2, y_cent - a/2]
+		x_edges = [x_cent - a/2, x_cent + a/2, x_cent + a/2, x_cent - a/2]
+		y_edges = [y_cent - a/2, y_cent - a/2, y_cent + a/2, y_cent + a/2]
 		super().__init__(eps, x_edges, y_edges)
