Fixed multi-layer normalization and grating_gme.py

examples/grating_gme.py
@@ -3,8 +3,8 @@ import numpy as np
 
 import pygme
 
-y_max = 1.0
-gmax = 15
+gmax = 5
+y_max = 1e-1 # Generally anything < 1/gmax is fine, too small is bad for visualization
 
 W = 0.5
 H = 1 / 3
@@ -13,7 +13,7 @@ D = 0.1
 epsr_bot = 1.07
 epsr_top = 11.25
 
-lattice = pygme.Lattice([1, 0], [0, 1])
+lattice = pygme.Lattice([1, 0], [0, y_max])
 phc = pygme.PhotCryst(lattice)
 
 # Substrate
@@ -22,17 +22,18 @@ phc.add_layer(d=D, eps_b=epsr_bot)
 # Grating
 phc.add_layer(d=H, eps_b=1)
 
-grating = pygme.Poly(eps=epsr_top, x_edges=[-W / 2, -W / 2, +W / 2, +W / 2], y_edges=[0.5, -0.5, -0.5, 0.5])
+grating = pygme.Poly(eps=epsr_top, x_edges=[-W / 2, -W / 2, +W / 2, +W / 2], y_edges=np.array([0.5, -0.5, -0.5, 0.5])*y_max)
 phc.layers[-1].add_shape(grating)
 
-gme = pygme.GuidedModeExp(phc, gmax=7)
-gme.plot_overview_ft(Ny=500)
+gme = pygme.GuidedModeExp(phc, gmax=gmax)
+# gme.plot_overview_ft()
+# phc.plot_overview()
 
 path = phc.lattice.bz_path(['G', np.array([np.pi, 0])], [50])
 
-gme.run(kpoints=path.kpoints, gmode_inds=[0], N_g_array=500, verbose=False)
+gme.run(kpoints=path.kpoints, gmode_inds=[0], N_g_array=500, verbose=False, numeig=10)
 
 fig, ax = plt.subplots(1, constrained_layout=True)
-plt.plot(gme.freqs, 'o-')
-ax.set_xlim([0, gme.freqs.shape[0] - 1])
+plt.plot(path.kpoints[0, :], gme.freqs, 'o')
+ax.set_xlim([0, path.kpoints[0, -1]])
 plt.show()

pygme/gme.py
@@ -159,7 +159,7 @@ class GuidedModeExp(object):
 			- compute the real eigenvalues and corresponding eigenvectors
 		'''
 
-		# Bloch momenta over which band structure is imulated 
+		# Bloch momenta over which band structure is simulated 
 		self.kpoints = kpoints
 		# Indexes of modes to be included in the expansion
 		gmode_inds = np.array(gmode_inds)
@@ -177,7 +177,7 @@ class GuidedModeExp(object):
 		Gmax = np.amax(np.sqrt(np.square(self.gvec[0, :]) +
 							np.square(self.gvec[1, :])))
 		# Array of g-points over which the guided modes will be computed
-		g_array = np.linspace(1e-6, Gmax + kmax, N_g_array)
+		g_array = np.linspace(1e-3, Gmax + kmax, N_g_array)
 		# Array of average permittivity of every layer (including claddings)
 		eps_array = np.array(list(get_value(layer.eps_avg) for layer in \
 			[self.phc.claddings[0]] + self.phc.layers + 
@@ -198,7 +198,7 @@ class GuidedModeExp(object):
 		if self.gmode_te.size > 0:
 			(omegas_te, coeffs_te) = guided_modes(g_array, eps_array, d_array, 
 						step=gmode_step, n_modes=1 + np.amax(self.gmode_te)//2, 
-						tol=1e-4, mode='TE')
+						tol=1e-10, mode='TE')
 			self.omegas_te = reshape_list(omegas_te)
 			self.coeffs_te = reshape_list(coeffs_te)
 		else:
@@ -208,7 +208,7 @@ class GuidedModeExp(object):
 		if self.gmode_tm.size > 0:
 			(omegas_tm, coeffs_tm) = guided_modes(g_array, eps_array, d_array, 
 						step=gmode_step, n_modes=1 + np.amax(self.gmode_tm)//2, 
-						tol=1e-4, mode='TM')
+						tol=1e-10, mode='TM')
 			self.omegas_tm = reshape_list(omegas_tm)
 			self.coeffs_tm = reshape_list(coeffs_tm)
 		else:
@@ -300,6 +300,7 @@ class GuidedModeExp(object):
 			gs = self.g_array[-len(omegas[im]):]
 			indmode = np.argwhere(gk > gs[0]).ravel()
 			oms = np.interp(gk[indmode], gs, omegas[im])
+			# prit('k=' )
 
 			As, Bs, chis = (np.zeros((self.N_layers + 2, 
 					indmode.size), dtype=np.complex128) for i in range(3))

pygme/guided_modes_new.py
@@ -79,10 +79,6 @@ def guided_mode_given_g(g, eps_array, d_array, n_modes=1,
 	omega_bounds = np.append(np.arange(omega_lb + tol, omega_ub - tol, step), 
 					omega_ub-tol) 
 
-	# Some special care for when g is close to zero
-	if omega_bounds.size < 2:
-		omega_bounds = np.linspace(omega_lb+1e-15, omega_ub-1e-15, 2)
-
 	omega_solutions = [] ## solving for D22_real
 	coeffs = []
 
@@ -103,7 +99,8 @@ def guided_mode_given_g(g, eps_array, d_array, n_modes=1,
 								chi_array, mode)
 			norm = normalization_coeff(omega, g, eps_array, d_array, 
 								AB, mode)
-
+			# print(norm)
+ 
 			coeffs.append(AB / np.sqrt(norm))
 		else:
 			raise ValueError("Mode should be 'TE' or 'TM'")
@@ -319,11 +316,14 @@ def AB_matrices(omega, g, eps_array, d_array, chi_array=None, mode='TE'):
 	return np.array(ABs)
 
 def normalization_coeff(omega, g, eps_array, d_array, ABref, mode='TE'):
+	'''
+	Normalization of the guided modes (i.e. the A and B coeffs)
+	'''
 	assert len(d_array)==len(eps_array)-2, \
 		'd_array should have length = num_layers'
 	chi_array = chi(omega, g, eps_array)
-	As = ABref[:, 0]
-	Bs = ABref[:, 1]
+	As = ABref[:, 0].ravel()
+	Bs = ABref[:, 1].ravel()
 	if mode == 'TM': 
 		term1 = eps_array[0]**2 / np.abs(chi_array[0])**2 * omega**2 * \
 			(np.abs(Bs[0])**2) * J_alpha(chi_array[0]-chi_array[0].conj())