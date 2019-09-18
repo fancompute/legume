6d1410213a15bdcf42f44898c4cdb92f7e3bb68a
Ian Williamson <ian.williamson@ieee.org>
2019-09-18 15:05:35 -0700

    Fixed options args in grating examples

--- examples/grating_gme.py
+++ examples/grating_gme.py
@@ -38,9 +38,9 @@ if args.overview:
 path = phc.lattice.bz_path(['G', np.array([np.pi, 0])], [35])
 options = {'gmode_inds': np.arange(0, 8), 'gmode_npts': 500, 'numeig': args.neig, 'verbose': False}
 
-gme.run(kpoints=path.kpoints, options=options)
+gme.run(kpoints=path.kpoints, **options)
 
 ax = pygme.viz.bands(gme)
-data = np.loadtxt('./examples/filtered_symmetric_H0.25_W0.50.csv', comments='%', delimiter=',')
+# data = np.loadtxt('./examples/filtered_symmetric_H0.25_W0.50.csv', comments='%', delimiter=',')
 # data = np.loadtxt('./examples/grating_bands.csv', comments='%', delimiter=',')
-ax.plot(data[:, 0], data[:, 1:], 'o', markeredgecolor='k', color='none', label="COMSOL")
+# ax.plot(data[:, 0], data[:, 1:], 'o', markeredgecolor='k', color='none', label="COMSOL")
