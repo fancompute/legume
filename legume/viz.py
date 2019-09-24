09d84c6a3b504fed466307c786b83bf6e06038ce
Ian Williamson <ian.williamson@ieee.org>
2019-09-24 12:12:49 -0700

    Improved band plotting somewhat and fixed bug in light line

--- legume/viz.py
+++ legume/viz.py
@@ -3,22 +3,26 @@ import numpy as np
 
 
 # TODO: Make this more general
-def bands(gme, lightcone=True, ax=None, figsize=(4,5)):
+def bands(gme, lightcone=True, ax=None, figsize=(4,5), ls='o-'):
 
-    X = np.arange(len(gme.kpoints[0, :]))
+    if np.all(gme.kpoints[0,:]==0) and not np.all(gme.kpoints[1,:]==0) \
+        or np.all(gme.kpoints[1,:]==0) and not np.all(gme.kpoints[0,:]==0):
+        X = np.sqrt(np.square(gme.kpoints[0,:]) + np.square(gme.kpoints[1,:])) / np.pi
+    else:
+        X = np.arange(len(gme.kpoints[0, :]))
 
     if ax is None:
         fig, ax = plt.subplots(1, 1, constrained_layout=True, figsize=figsize)
 
-    plt.plot(X, gme.freqs, 'o', c="#1f77b4", label="", ms=4, mew=1)
+    plt.plot(X, gme.freqs, ls, c="#1f77b4", label="", ms=4, mew=1)
 
     if lightcone:
-        vec_LL = np.sqrt(np.square(gme.kpoints[0, :]) + np.square(gme.kpoints[1, :])) / np.pi /2
+        vec_LL = np.sqrt(np.square(gme.kpoints[0, :]) + np.square(gme.kpoints[1, :])) / np.pi
         ax.fill_between(X, vec_LL, gme.freqs[:].max(), facecolor="#cccccc", zorder=4, alpha=0.5)
 
-    ax.set_xlim(left=0, right=len(gme.kpoints[0, :])-1)
+    ax.set_xlim(left=0, right=max(X))
     ax.set_ylim(bottom=0.0, top=gme.freqs[:].max())
-    ax.set_xticks([])
+    # ax.set_xticks([])
     ax.set_xlabel('Wave vector')
     ax.set_ylabel('Frequency')
 
