11ee25c67ee591f80fb470ca68bbffd09ca7ed20
Ian Williamson <ian.williamson@ieee.org>
2019-09-25 16:49:56 -0700

    Further parameterized and cleaned up structure

--- examples/opt_grating_polygons.py
+++ examples/opt_grating_polygons.py
@@ -3,33 +3,44 @@
 
 import argparse
 
-import numpy as np
 import autograd
+import matplotlib.pyplot as plt
+import numpy as np
 from autograd import grad
 
 import legume
 from legume.backend import backend as bd
 from legume.optimizers import adam_optimize
-import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--verbose', action='store_true')
+parser.add_argument('--optimize', action='store_true')
+parser.add_argument('-N_epochs', default=10, type=int)
+parser.add_argument('-learning_rate', default=0.1, type=float)
 parser.add_argument('-gmax', default=5, type=int)
+parser.add_argument('-gmode_npts', default=1000, type=int)
 parser.add_argument('-neig', default=10, type=int)
 parser.add_argument('-ymax', default=0.1, type=float)
-parser.add_argument('-N', default=20, type=int)
+parser.add_argument('-N_polygons', default=20, type=int)
 parser.add_argument('-H', default=0.33, type=float)
 parser.add_argument('-D', default=0.20, type=float)
 parser.add_argument('-eps', default=12, type=float)
 args = parser.parse_args()
 
+options = {'gmode_inds': np.arange(0, 8, 2),
+		   'gmode_npts': args.gmode_npts,
+		   'numeig': args.neig,
+		   'verbose': args.verbose}
+
+lattice = legume.Lattice([1, 0], [0, args.ymax])
+path = lattice.bz_path(['G', np.array([np.pi / 2, 0])], [35])
+
 
 def projection(rho, eta=0.5, beta=100):
-	return bd.abs(rho)
-	# return bd.divide(bd.tanh(beta * eta) + bd.tanh(beta * (rho - eta)), bd.tanh(beta * eta) + bd.tanh(beta * (1 - eta)))
+	return bd.divide(bd.tanh(beta * eta) + bd.tanh(beta * (rho - eta)), bd.tanh(beta * eta) + bd.tanh(beta * (1 - eta)))
 
 
 def make_grating():
-	lattice = legume.Lattice([1, 0], [0, args.ymax])
 	phc = legume.PhotCryst(lattice)
 	phc.add_layer(d=args.D, eps_b=args.eps)
 	phc.add_layer(d=args.H, eps_b=1)
@@ -50,46 +61,48 @@ def parameterize_density_layer(layer, rho, eta=0.5, beta=100):
 
 
 def objective(rho):
-	parameterize_density_layer(gme.phc.layers[-1], rho, eta=0.5, beta=100)
+	parameterize_density_layer(gme.phc.layers[-1], rho, eta=0.5, beta=10)
 	gme.run(kpoints=path.kpoints, **options)
 	tgt_freqs = gme.freqs[1:10, 1]
 	return bd.sqrt(bd.var(tgt_freqs))
 
 
-# Initialize
-rho_0 = np.zeros((args.N,)) + 0.1
-rho_0[int(0.25 * len(rho_0)):int(0.75 * len(rho_0))] = 1.0
+# Initialize structure
 
-# Compute initial results
+# rho_0 = np.zeros((args.N_polygons,)) + 0.1
+# rho_0[int(0.25 * len(rho_0)):int(0.75 * len(rho_0))] = 1.0
+
+x0 = np.linspace(-np.pi, +np.pi, args.N_polygons)
+rho_0 = 0.5 + np.sin(x0) * 0.5
+
+# Compute results for initial structure
 legume.set_backend('autograd')
 objective_grad = grad(objective)
 gme = make_grating()
-path = gme.phc.lattice.bz_path(['G', np.array([np.pi, 0])], [35])
-options = {'gmode_inds': np.arange(0, 8, 2), 'gmode_npts': 500, 'numeig': args.neig, 'verbose': False}
-parameterize_density_layer(gme.phc.layers[-1], rho_0, eta=0.5, beta=100)
+parameterize_density_layer(gme.phc.layers[-1], rho_0, eta=0.5, beta=10)
 gme.run(kpoints=path.kpoints, **options)
-gme.phc.plot_overview()
+gme.phc.plot_overview(cmap='Reds')
 legume.viz.bands(gme)
 
-## Optimize
-legume.set_backend('autograd')
-(rho_opt, ofs) = adam_optimize(objective, rho_0, objective_grad, step_size=1e-2, Nsteps=10,
-							   options={'direction': 'min', 'disp': ['of', 'params']})
-legume.set_backend('numpy')
+if args.optimize:
+	## Optimize
+	legume.set_backend('autograd')
+	(rho_opt, ofs) = adam_optimize(objective, rho_0, objective_grad, step_size=args.learning_rate, Nsteps=args.N_epochs,
+								   options={'direction': 'min', 'disp': ['of', 'params']})
+	legume.set_backend('numpy')
 
-of_value = [of._value if type(of) is autograd.numpy.numpy_boxes.ArrayBox else of for of in ofs]
+	of_value = [of._value if type(of) is autograd.numpy.numpy_boxes.ArrayBox else of for of in ofs]
 
-plt.figure(figsize=(4,3), constrained_layout=True)
-plt.plot(of_value, "o-")
-plt.xlabel("Epoch")
-plt.ylabel("Cost function")
-plt.show()
+	plt.figure(figsize=(4, 3), constrained_layout=True)
+	plt.plot(of_value, "o-")
+	plt.xlabel("Epoch")
+	plt.ylabel("Cost function")
+	plt.show()
 
-## Display results (recompute at optimal structure)
-gme = make_grating()
-path = gme.phc.lattice.bz_path(['G', np.array([np.pi, 0])], [35])
-parameterize_density_layer(gme.phc.layers[-1], rho_opt, eta=0.5, beta=100)
-gme.run(kpoints=path.kpoints, **options)
+	## Display results (recompute at optimal structure)
+	gme = make_grating()
+	parameterize_density_layer(gme.phc.layers[-1], rho_opt, eta=0.5, beta=10)
+	gme.run(kpoints=path.kpoints, **options)
 
-gme.phc.plot_overview()
-legume.viz.bands(gme)
+	gme.phc.plot_overview(cmap='Blues')
+	legume.viz.bands(gme)
