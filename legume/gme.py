Fixing some edge cases of the Q computation

legume/gme.py
@@ -495,9 +495,9 @@ class GuidedModeExp(object):
 				"stored eigenmodes" % self.numeig)
 		
 		# G + k vectors
-		gkx = self.gvec[0, :] + self.kpoints[0, kind]
+		gkx = self.gvec[0, :] + self.kpoints[0, kind] + 1e-10
 		gky = self.gvec[1, :] + self.kpoints[1, kind]
-		gk = np.sqrt(np.square(gkx + 1e-20) + np.square(gky))
+		gk = np.sqrt(np.square(gkx) + np.square(gky))
 
 		# Iterate over all the modes to be computed
 		rad_tot = []
@@ -572,13 +572,12 @@ class GuidedModeExp(object):
 						rad = self.rad_tm_tm(gk, indmode1, oms1, As1, Bs1, 
 							chis1, indmoder[clad_ind], omr, Xs['tm'][clad_ind], 
 							Ys['tm'][clad_ind], chis['tm'][clad_ind], pp)
-					# raise Exception
+
 					rad = rad*bd.conj(evec[count:
 						count+self.modes_numg[kind][im1]][:, np.newaxis])
-					rad_coup['tm'][clad_ind] += bd.sum(rad, axis=0)
-					# raise Exception
+					rad_coup['tm'][clad_ind] = rad_coup['tm'][clad_ind] + bd.sum(rad, axis=0)
+
 				count += self.modes_numg[kind][im1]
-			# raise Exception
 			rad_dos = [self.phc.claddings[i].eps_avg/bd.sqrt(
 					self.phc.claddings[i].eps_avg*omr**2 - gkr[i]**2) / \
 					4 / np.pi for i in [0, 1]]

legume/slab_modes.py
@@ -389,17 +389,18 @@ def rad_modes(omega, g_array, eps_array, d_array, pol='TE', clad=0):
 	Xs = np.zeros((eps_array.size, g_array.size), dtype=np.complex128)
 	Ys = np.zeros((eps_array.size, g_array.size), dtype=np.complex128)
 	for ig, g in enumerate(g_array):
+		g_val = max([g, 1e-10])
 		# Get the scattering and transfer matrices
 		if pol.lower()=='te' and clad==0:
-			S_mat, T_mat = S_T_matrices_TE(omega, g, np.flip(eps_array), 
+			S_mat, T_mat = S_T_matrices_TE(omega, g_val, np.flip(eps_array), 
 							np.flip(d_array))
 		elif pol.lower()=='te' and clad==1:
-			S_mat, T_mat = S_T_matrices_TE(omega, g, eps_array, d_array)
+			S_mat, T_mat = S_T_matrices_TE(omega, g_val, eps_array, d_array)
 		elif pol.lower()=='tm' and clad==0:
-			S_mat, T_mat = S_T_matrices_TM(omega, g, np.flip(eps_array), 
+			S_mat, T_mat = S_T_matrices_TM(omega, g_val, np.flip(eps_array), 
 							np.flip(d_array))
 		elif pol.lower()=='tm' and clad==1:
-			S_mat, T_mat = S_T_matrices_TM(omega, g, eps_array, d_array)
+			S_mat, T_mat = S_T_matrices_TM(omega, g_val, eps_array, d_array)
 		
 		# Compute the transfer matrix coefficients
 		coeffs = np.zeros((2, eps_array.size), dtype=np.complex128)

tests/test_gme_rect.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 
 	gme = GuidedModeExp(phc, gmax=3)
 	# gme.plot_overview_ft(cladding=True)
-	options = {'gmode_inds': [1, 2], 'numeig': 10, 'verbose': True, 'gmode_npts': 2000}
-	gme.run(kpoints=np.array([[0.1], [0.2]]), **options)
+	options = {'gmode_inds': [0, 1, 2], 'numeig': 10, 'verbose': True, 'gmode_npts': 2000}
+	gme.run(kpoints=np.array([[0.], [0.]]), **options)
 	print(gme.freqs)
-	(freqs_im, coup_l, coup_u) = gme.compute_rad(kind=0, minds=[1, 2, 3])
+	(freqs_im, coup_l, coup_u) = gme.compute_rad(kind=0, minds=list(range(5)))
 	print(freqs_im)