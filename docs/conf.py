db5d70d2b1bf7749ab2ac40401095819480550ed
Ian Williamson <ian.williamson@ieee.org>
2020-02-17 12:31:54 -0800

    Added napolean configuration for Numpy style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.extlinks",
     "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
     "nbsphinx",
@@ -51,6 +52,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 autosummary_generate = True
 autodoc_typehints = "none"
 
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
 
