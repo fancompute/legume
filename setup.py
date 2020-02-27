36e6633819ec0294ddb9609545fb4e71cbc522c1
Ian Williamson <ian.williamson@ieee.org>
2020-02-27 14:18:49 -0800

    setup.py single source the reqs and package version

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
+import os
+import re
+import codecs
 from setuptools import setup, find_packages
+
+# Single-source the version from legume/__init__.py
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+#
 
 with open('README.md', 'r') as f:
     readme = f.read()
 
-dependencies = [
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'autograd'
-]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='legume-gme',
-    version='0.0.2',
+    version=find_version('legume', '__init__.py'),
     description='Differentiable plane-wave and guided-mode expansion for photonic crystals',
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -20,7 +35,7 @@ setup(
     author_email='momchilmm@gmail.com',
     url='https://github.com/fancompute/legume',
     packages=find_packages(),
-    install_requires=dependencies,
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
